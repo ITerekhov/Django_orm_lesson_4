9e6bb932cf00de6fd1eea3f2456c99e267acfcff
foxigor98@mail.ru <foxigor@foxigor.localdomain>
2022-04-27 17:30:01 +0300

    Made the image a mandatory field in the pokemon`s model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Pokemon(models.Model):
     '''Породы покемонов'''
     title = models.CharField('Имя', max_length=200)
-    image = models.ImageField('Изображение', null=True, blank=True)
+    image = models.ImageField('Изображение')
     description = models.TextField('Описание', blank=True)
     title_en = models.CharField('Имя на англисйком', max_length=200, blank=True)
     title_jp = models.CharField('Имя на японском', max_length=200, blank=True)
