21bb4d0b8d589ca6918dea64aa16136a901a486f
foxigor98@mail.ru <foxigor@foxigor.localdomain>
2022-04-27 17:09:19 +0300

    Add verbose names to all fields in DB

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,26 +1,30 @@
 from django.db import models 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.ImageField(null=True, blank=True)
-    description = models.TextField()
-    title_en = models.CharField(max_length=200)
-    title_jp = models.CharField(max_length=200)
+    '''Породы покемонов'''
+    title = models.CharField('Имя', max_length=200)
+    image = models.ImageField('Изображение', null=True, blank=True)
+    description = models.TextField('Описание')
+    title_en = models.CharField('Имя на англисйком', max_length=200)
+    title_jp = models.CharField('Имя на японском', max_length=200)
     previous_evolution = models.ForeignKey("self", on_delete=models.SET_NULL, 
                                related_name='next_evolutions',
+                               verbose_name='Предыдущее поколение',
                                null=True)
     def __str__(self):
         return self.title
 
 class PokemonEntity(models.Model):
+    '''Отдельные экземпляры покемонов'''
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE,
-                                related_name='endities')
-    lat = models.FloatField()
-    lon = models.FloatField()
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
-    level = models.IntegerField()
-    health = models.IntegerField()
-    strength = models.IntegerField()
-    defence = models.IntegerField()
-    stamina = models.IntegerField()
+                                related_name='endities',
+                                verbose_name='Порода покемона')
+    lat = models.FloatField('Координаты широты')
+    lon = models.FloatField('Координаты долготы')
+    appeared_at = models.DateTimeField('Дата появления')
+    disappeared_at = models.DateTimeField('Дата исчезновения')
+    level = models.IntegerField('Уровень')
+    health = models.IntegerField('Здоровье')
+    strength = models.IntegerField('Сила')
+    defence = models.IntegerField('Защита')
+    stamina = models.IntegerField('Выносливость')
