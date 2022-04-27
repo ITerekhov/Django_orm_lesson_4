765f4ef72e1a5a27a2f384846caba81b0109cf72
foxigor98@mail.ru <foxigor@foxigor.localdomain>
2022-04-27 13:42:43 +0300

    Add PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True )
 
     def __str__(self):
         return self.title
+
+class PokemonEntity(models.Model):
+    lat: models.FloatField()
+    lon: models.FloatField()
