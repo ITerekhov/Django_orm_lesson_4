220f4dd72eb259582b9ac752fc069f5ae6a94d7d
foxigor98@mail.ru <foxigor@foxigor.localdomain>
2022-04-27 17:23:18 +0300

    Made fields in models optional where appropriate

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,13 +4,14 @@ class Pokemon(models.Model):
     '''Породы покемонов'''
     title = models.CharField('Имя', max_length=200)
     image = models.ImageField('Изображение', null=True, blank=True)
-    description = models.TextField('Описание')
-    title_en = models.CharField('Имя на англисйком', max_length=200)
-    title_jp = models.CharField('Имя на японском', max_length=200)
+    description = models.TextField('Описание', blank=True)
+    title_en = models.CharField('Имя на англисйком', max_length=200, blank=True)
+    title_jp = models.CharField('Имя на японском', max_length=200, blank=True)
     previous_evolution = models.ForeignKey("self", on_delete=models.SET_NULL, 
                                related_name='next_evolutions',
                                verbose_name='Предыдущее поколение',
-                               null=True)
+                               null=True,
+                               blank=True)
     def __str__(self):
         return self.title
 
@@ -21,10 +22,10 @@ class PokemonEntity(models.Model):
                                 verbose_name='Порода покемона')
     lat = models.FloatField('Координаты широты')
     lon = models.FloatField('Координаты долготы')
-    appeared_at = models.DateTimeField('Дата появления')
+    appeared_at = models.DateTimeField('Дата появления', null=True, blank=True)
     disappeared_at = models.DateTimeField('Дата исчезновения')
-    level = models.IntegerField('Уровень')
-    health = models.IntegerField('Здоровье')
-    strength = models.IntegerField('Сила')
-    defence = models.IntegerField('Защита')
-    stamina = models.IntegerField('Выносливость')
+    level = models.IntegerField('Уровень', null=True, blank=True)
+    health = models.IntegerField('Здоровье', null=True, blank=True)
+    strength = models.IntegerField('Сила', null=True, blank=True)
+    defence = models.IntegerField('Защита', null=True, blank=True)
+    stamina = models.IntegerField('Выносливость', null=True, blank=True)
